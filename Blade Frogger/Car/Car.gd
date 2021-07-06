c9045ca51adf390969f5bac956763d569cfc4836
Lesedi <leseditumelo32@gmail.com>
2021-07-06 10:56:37 +0200

    spawning multiple cars

---
+++ Blade Frogger/Car/Car.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var car_speed = 250
+
+func _process(delta):
+	position.x -= car_speed * delta
+
+
+func _on_Car_body_entered(body):
+	get_tree().change_scene("res://GameScreen/GameScreen.tscn")
